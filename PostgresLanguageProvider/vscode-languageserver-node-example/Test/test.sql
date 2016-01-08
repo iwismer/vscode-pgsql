bbe1a10439cde4e2522cb5057cf7fac18fa26897
Juan Tarquino <jtarquino@CSBFL.LOCAL>
2016-01-08 14:59:07 -0500

    Initial

---
+++ PostgresLanguageProvider/vscode-languageserver-node-example/Test/test.sql
@@ -0,0 +1,5 @@
+typescr lets you write JavaScript the way you really want to.
+typescript is a typed superset of JavaScript that compiles to plain JavaScript.
+Any browser. Any host. Any OS. Open Source. JavaScript() 
+
+  FROM(3,1)
---
+++ PostgresLanguageProvider/vscode-languageserver-node-example/client/src/features/completionProvider.ts
@@ -0,0 +1,74 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+'use strict';
+
+import { CompletionItem, TextDocument, Position, CompletionItemKind, CompletionItemProvider, CancellationToken } from 'vscode';
+
+
+import { CompletionEntry, CompletionsRequestArgs, CompletionsResponse, CompletionDetailsRequestArgs, CompletionDetailsResponse, CompletionEntryDetails } from '../protocol';
+
+
+class MyCompletionItem extends CompletionItem {
+
+	document: TextDocument;
+	position: Position;
+
+	constructor(entry: CompletionEntry) {
+		super(entry.name);
+		this.sortText = entry.sortText;
+		this.kind = MyCompletionItem.convertKind(entry.kind);
+	}
+
+	private static convertKind(kind: string): CompletionItemKind {
+		
+
+		return CompletionItemKind.Property;
+	}
+}
+
+export default class PostgresqlCompletionItemProvider implements CompletionItemProvider {
+
+	public triggerCharacters = ['.', ' '];
+	public excludeTokens = ['string', 'comment', 'numeric'];
+	public sortBy = [{ type: 'reference', partSeparator: '/' }];
+
+/*
+	private client: ITypescriptServiceClient;
+	private config: IConfiguration;
+
+	constructor(client: ITypescriptServiceClient) {
+		this.client = client;
+		this.config = defaultConfiguration;
+	}
+
+	public setConfiguration(config: IConfiguration): void {
+		this.config = config;
+	}
+*/
+    private createKeywordCompletionItem(keyword: string)
+    {
+        var item = new CompletionItem(keyword);
+        item.kind = CompletionItemKind.Keyword
+        return item;
+    }
+	public provideCompletionItems(document: TextDocument, position: Position, token: CancellationToken): Promise<CompletionItem[]> {
+		let filepath = document.uri;
+        let line = position.line + 1;
+		let offset = position.character + 1;
+        
+		if (!filepath) {
+			return Promise.resolve<CompletionItem[]>([]);
+		}
+        var testKeywords = [ this.createKeywordCompletionItem( 'SELECT'),this.createKeywordCompletionItem( 'FROM')  ];
+		return Promise.resolve<CompletionItem[]>(testKeywords); ;
+	}
+
+	public resolveCompletionItem(item: CompletionItem, token: CancellationToken): any | Thenable<any> {
+		return item;
+
+		
+	}
+}
